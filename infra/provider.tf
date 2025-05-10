b0e07e6756d017615aebbf122c746bb7838ce698
Madhusudan V B <madhusudanvb25@gmail.com>
2025-05-10 17:39:28 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                   = "ap-south-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+  shared_credentials_files = ["/Path/to/aws/credentials"]
 }
